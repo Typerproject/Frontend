04fca7d1e3fea944c198a942fe7a6599ff3c1b7d
kim nahyeon <stome6322@inha.edu>
2024-06-09 01:56:28 +0900

    ✨ feat: 타입 안정성을 위한 커스텀 훅 추가
    주석도 함께 추가하였습니다.

--- frontend/src/store/index.tsx
+++ frontend/src/store/index.tsx
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./reducers/user";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 const store = configureStore({
   reducer: {
@@ -12,6 +13,9 @@ const store = configureStore({
   },
 });
 
-export type RootState = ReturnType<typeof store.getState>; //useSelect 사용 시 state 받을 때 쓰는 타입
-
+export const useAppDispatch:() => typeof store.dispatch=useDispatch;
+// const dispatch = useAppDispatch(); 로 사용
+export const useAppSelector:TypedUseSelectorHook<ReturnType<typeof store.getState>> = useSelector;
+// const userInfo = useAppSelector(state=>state.user); 로 사용하기
+// export type RootState = ReturnType<typeof store.getState>; //useSelect 사용 시 state 받을 때 쓰는 타입
 export default store;
