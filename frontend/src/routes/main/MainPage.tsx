2eb83d5beb98001a55579c6e8264230f0e9a09d4
kim nahyeon <stome6322@inha.edu>
2024-06-10 09:59:36 +0900

    ✨ feat: axios 사용 예시 코드 추가

--- frontend/src/routes/main/MainPage.tsx
+++ frontend/src/routes/main/MainPage.tsx
@@ -1,8 +1,17 @@
 // import React from 'react'
+import { useEffect } from "react";
+import postDetailAPI from "../../api/postDetailAPI";
 
 // type Props = {}
 
 export default function MainPage() {
+  const service = new postDetailAPI(import.meta.env.VITE_BASE_URI);
+  useEffect(()=>{
+    service.getPostInfo("id어쩌구저쩌구")
+    .then(res=>console.log(res))
+    .catch(e=>console.log(e));
+  }, []);
+  
   return (
     <div
     className="bg-red-500"
