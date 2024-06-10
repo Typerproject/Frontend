6a34b40e8f4772fdab5e5129b34ebea427fb6602
kim nahyeon <stome6322@inha.edu>
2024-06-11 08:05:31 +0900

    🎨 design: Comment, InputComment style 수정

--- frontend/src/routes/postDetail/component/Comment.tsx
+++ frontend/src/routes/postDetail/component/Comment.tsx
@@ -8,7 +8,7 @@ export default function Comment() {
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTD68cSMsrBiEs6YloK8MVPO1DlJ7LqKt4OxT7ioMJn7xh-1iqPV0FVFjvTA7Cvlv-Y9Yc&usqp=CAU"
       />
       <p className="text-sm">댓글 단 user</p>
-      <div className="relative grow bg-gray-300 rounded-full px-[1.25rem] py-[0.75rem]">
+      <div className="relative grow bg-gray-300 rounded-full pl-[1.25rem] pr-[4.25rem] py-[0.75rem]">
         <p>오 글 좋네요 하트 누름^_^</p>
         <button className="absolute right-[0px] top-[50%] translate-y-[-50%] translate-x-[-25%] bg-black text-white rounded-full border-[1px] border-black text-xs px-[0.7rem] py-[0.3rem] hover:bg-white hover:text-black duration-300">
           삭제
--- frontend/src/routes/postDetail/component/InputComment.tsx
+++ frontend/src/routes/postDetail/component/InputComment.tsx
@@ -11,7 +11,7 @@ export default function InputComment() {
       />
       <p className="text-sm">로그인 한 user</p>
       <div className="grow relative">
-        <input className="w-full bg-gray-300 rounded-full px-[1.25rem] py-[0.75rem]" />
+        <input className="w-full bg-gray-300 rounded-full pl-[1.25rem] pr-[4.25rem] py-[0.75rem]" />
         <button className="absolute right-[0px] top-[50%] translate-y-[-50%] translate-x-[-25%] bg-black text-white rounded-full border-[1px] border-black text-xs px-[0.7rem] py-[0.3rem] hover:bg-white hover:text-black duration-300">등록</button>
       </div>
     </div>
