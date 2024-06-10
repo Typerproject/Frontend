7e57ac4f83e2d23a8aa40067ff6d09ee188ef2f0
kim nahyeon <stome6322@inha.edu>
2024-06-10 15:49:19 +0900

    💬 comment: PostContent 주석 추가

--- frontend/src/routes/postDetail/component/PostContent.tsx
+++ frontend/src/routes/postDetail/component/PostContent.tsx
@@ -5,10 +5,16 @@ import WriterInfo from "./WriterInfo"
 export default function PostContent() {
   return (
     <div className="w-full h-full min-h-70 rounded-[10px] shadow-lg shadow-[0_0_8px_5px] shadow-gray-200 p-[2rem]">
+
+        {/* 글 제목, 날짜, 글쓴이 기본 정보 */}
         <h1 className="text-3xl	font-bold">게시글 상세 페이지</h1>
         <p className="text-gray-400 my-[10px]">2024년 06월 07일 13:23</p>
         <WriterInfo />
+
+        {/* divider */}
         <div className="h-[0.5px] my-[0.75rem] w-full bg-current"></div>
+
+        {/* 글 내용 */}
         <p className="py-[0.75rem]">글 내용 (따로 컴포로 뺄 예정)</p>
     </div>
   )
