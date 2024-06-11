e812c7ba3ec80587dad5477296eb90f42560c9fc
잰진 <p_star16@naver.com>
2024-06-11 14:33:21 +0900

    design: 프로필 간격 조절 및 좌측 정렬

--- frontend/src/routes/my/MyPage.tsx
+++ frontend/src/routes/my/MyPage.tsx
@@ -11,20 +11,25 @@ export default function MyPage() {
   const following = 53;
 
   return (
-    <div className="mt-[7rem] ">
+    <div className="mt-[7rem]">
       <div className="grid grid-cols-3">
         <div className="col-span-2 flex justify-center  text-5xl">
           <p>{userId}'s Typer</p>
         </div>
-        <div className="col-span-1 flex justify-center items-center">
-          <div className="flex flex-col items-center gap-5">
+        <div className="col-span-1">
+          <div>
             <img className="size-24 rounded-full" src={profileImg} />
-            <p className="text-3xl">{userId}</p>
-            <div className="flex gap-10 text-[#b1b2b3]">
+            <p className="text-3xl mt-[0.5rem] ">{userId}</p>
+            <div className="flex gap-10 text-[#b1b2b3] mt-[0.3rem]">
               <span>{follower} followers</span>
               <span>{following} following</span>
             </div>
-            <p className="flex gap-10 text-[#9d9d9e]">{profileIntro}</p>
+            <p className="flex gap-10 text-[#9d9d9e] mt-[0.5rem]">
+              {profileIntro}
+            </p>
+            <button className="text-xs mt-[0.5rem] border-[1px] bg-black text-white rounded-full border-black text-sm px-[0.7rem] py-[0.3rem] hover:bg-white hover:text-black duration-300">
+              팔로우
+            </button>
           </div>
         </div>
       </div>
