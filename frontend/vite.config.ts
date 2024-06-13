2657bd525a049cb8724fa3e88eee5edf1104e243
lemonticsoul <sjho714@naver.com>
2024-06-13 16:17:26 +0900

    ✨feat:애널리스트 cors 에러 해결및 build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/analyst': {
+        target: 'https://consensus.hankyung.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        configure: (proxy) => {
+          proxy.on('proxyReq', (proxyReq, req, res) => {
+            console.log('Proxying request:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, res) => {
+            console.log('Received response from target:', proxyRes.statusCode, req.url);
+          });
+        }
+      }
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('pdf.worker.js')) {
+            return 'pdf.worker';
+          }
+        },
+      },
+    },
+  },
+});
