8c4ccf0cb1f36cf0db48226f585898f6ce0e0a98
jody816 <81346079+jody816@users.noreply.github.com>
2024-06-19 08:54:18 +0900

    💄 style: 들여쓰기 적용 및 따옴표 수정

--- frontend/src/routes/main/MainPage.tsx
+++ frontend/src/routes/main/MainPage.tsx
@@ -1,25 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Navigator from "../../components/Navbar/Navbar"
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Navigator from "../../components/Navbar/Navbar";
 
 const MyComponent = () => {
   const [issue, setIssue] = useState<Array<Array<string>>>([]);
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [activeButton, setActiveButton] = useState('HOT');
-  const [post,setPost]=useState([]);
+  const [activeButton, setActiveButton] = useState("HOT");
+  const [post, setPost] = useState([]);
 
-    const handleClick = (buttonName:any) => {
-        setActiveButton(buttonName);
-    };
-
+  const handleClick = (buttonName: any) => {
+    setActiveButton(buttonName);
+  };
 
   useEffect(() => {
     const hi = async () => {
       try {
-        const response = await axios.get(import.meta.env.VITE_SERVER_SHINHAN_API_URI);
+        const response = await axios.get(
+          import.meta.env.VITE_SERVER_SHINHAN_API_URI
+        );
         setIssue(response.data);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
     hi();
@@ -40,13 +41,16 @@ const MyComponent = () => {
       <Navigator />
       <div className="flex justify-center items-center mt-20">
         <div className="flex border rounded-md p-4">
-          
           {issue.length > 0 && (
-            <div key={currentIndex} className="flex flex-col justify-center w-120 h-12">
-              <div className="text-center font-bold">{issue[currentIndex][0]}</div>
+            <div
+              key={currentIndex}
+              className="flex flex-col justify-center w-120 h-12"
+            >
+              <div className="text-center font-bold">
+                {issue[currentIndex][0]}
+              </div>
               <div className="text-center">{issue[currentIndex][1]}</div>
               <div className="text-center">{issue[currentIndex][2]}</div>
-              
             </div>
           )}
           <div className="absolute bottom-2 right-2 text-xs font-bold">
@@ -54,28 +58,39 @@ const MyComponent = () => {
           </div>
         </div>
       </div>
-      
+
       <div className="flex ml-10 gap-5">
-            <button
-                className={`relative px-3 py-2 ${activeButton === 'HOT' ? 'font-bold after:content-[""] after:absolute after:left-0 after:bottom-0 after:w-full after:h-1 after:bg-black' : ''}`}
-                onClick={() => handleClick('HOT')}
-            >
-                🔥HOT
-            </button>
-            <button
-                className={`relative px-3 py-2 ${activeButton === 'NEW' ? 'font-bold after:content-[""] after:absolute after:left-0 after:bottom-0 after:w-full after:h-1 after:bg-black' : ''}`}
-                onClick={() => handleClick('NEW')}
-            >
-                🎉NEW
-            </button>
-            <button
-                className={`relative px-3 py-2 ${activeButton === 'FOllOW' ? 'font-bold after:content-[""] after:absolute after:left-0 after:bottom-0 after:w-full after:h-1 after:bg-black' : ''}`}
-                onClick={() => handleClick('FOllOW')}
-            >
-                ❤️FOLLOW
-            </button>
-        </div>
-        
+        <button
+          className={`relative px-3 py-2 ${
+            activeButton === "HOT"
+              ? 'font-bold after:content-[""] after:absolute after:left-0 after:bottom-0 after:w-full after:h-1 after:bg-black'
+              : ""
+          }`}
+          onClick={() => handleClick("HOT")}
+        >
+          🔥HOT
+        </button>
+        <button
+          className={`relative px-3 py-2 ${
+            activeButton === "NEW"
+              ? 'font-bold after:content-[""] after:absolute after:left-0 after:bottom-0 after:w-full after:h-1 after:bg-black'
+              : ""
+          }`}
+          onClick={() => handleClick("NEW")}
+        >
+          🎉NEW
+        </button>
+        <button
+          className={`relative px-3 py-2 ${
+            activeButton === "FOllOW"
+              ? 'font-bold after:content-[""] after:absolute after:left-0 after:bottom-0 after:w-full after:h-1 after:bg-black'
+              : ""
+          }`}
+          onClick={() => handleClick("FOllOW")}
+        >
+          ❤️FOLLOW
+        </button>
+      </div>
     </div>
   );
 };
