d802ab37af894efb659dc7bedd199022bfc0fbb2
yapyap2 <wonwoo42@gmail.com>
2024-06-20 16:18:25 +0900

    fix: 블록 툴 이름 한글로 변경

--- frontend/src/routes/editor/component/blockTools/news/News.tsx
+++ frontend/src/routes/editor/component/blockTools/news/News.tsx
@@ -32,25 +32,26 @@ const StockNews: React.FC<StockNewsProps> = ({ newsData, setNewsData }) => {
     return (
       <input
         className="w-full border border-gray-400	rounded-md p-[0.5rem]"
-        placeholder="기사의 url을 붙여넣어 주세요!"
+        placeholder="url을 붙여넣어 주세요!"
         onChange={(e) => handlePaste(e)}
       />
     );
   }
 
   return (
-    <div 
-    onClick={()=> window.open(newsInfo.newsUrl)}
-    className="border border-gray-300 rounded-md p-[0.75rem] grid grid-cols-7 max-w-[100%] cursor-pointer my-[1rem]">
+    <div
+      onClick={() => window.open(newsInfo.newsUrl)}
+      className="border border-gray-300 rounded-md p-[0.75rem] grid grid-cols-7 max-w-[100%] cursor-pointer my-[1rem]"
+    >
       <div className="col-[1/6]">
         <p className="font-semibold	mb-[5px]">{newsInfo.title}</p>
         <p className="text-sm truncate mb-[5px]">{newsInfo.detail}</p>
         <p className="text-sm	text-gray-500 truncate">{newsInfo.newsUrl}</p>
       </div>
-      <div className="col-[6/8] bg-cover bg-center ml-[5px]"
-      style={{ backgroundImage: `url(${newsInfo.imgUrl})` }}
-      >
-      </div>
+      <div
+        className="col-[6/8] bg-cover bg-center ml-[5px]"
+        style={{ backgroundImage: `url(${newsInfo.imgUrl})` }}
+      ></div>
     </div>
   );
 };
