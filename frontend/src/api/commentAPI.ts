b78910d8f3aac1cf9ede14247c03f7403960fdcc
kim nahyeon <stome6322@inha.edu>
2024-06-21 08:01:44 +0900

    ✨ feat: comment 관련 api 호출 코드 추가

---
+++ frontend/src/api/commentAPI.ts
@@ -0,0 +1,36 @@
+import BaseApi from "./axiosInstance";
+
+interface ICommentReq {
+  text: string;
+  postId: string;
+}
+
+interface IReplyReq extends ICommentReq {
+  parentCommentId: string;
+}
+
+export default class commentAPI extends BaseApi {
+  async postComment(req: ICommentReq) {
+    const resp = await this.fetcher.post(`/comment`, {
+      text: req.text,
+      postId: req.postId,
+    });
+
+    return resp;
+  }
+
+  async postReply(req: IReplyReq) {
+    const resp = await this.fetcher.post(`/comment/reply`, {
+      text: req.text,
+      postId: req.postId,
+      parentCommentId: req.parentCommentId,
+    });
+
+    return resp.data;
+  }
+
+  async deleteComment(commentId:string) {
+    const resp = await this.fetcher.delete(`/comment/${commentId}`);
+    return resp;
+  }
+}
