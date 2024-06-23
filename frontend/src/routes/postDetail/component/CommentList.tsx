768457bb45f19f64d87951195d7222d266043e3f
kim nahyeon <stome6322@inha.edu>
2024-06-23 14:22:50 +0900

    🎨 design: 댓글, 대댓글 새로운 디자인으로 바꿈

--- frontend/src/routes/postDetail/component/CommentList.tsx
+++ frontend/src/routes/postDetail/component/CommentList.tsx
@@ -3,13 +3,21 @@ import Comment from "./Comment";
 
 export default function CommentList() {
   return (
-    <div className="my-[2rem]">
-      {/* <p>코코</p> */}
+    <div className="mb-[5rem]">
+      <p>댓글 14</p>
+      <hr className="my-[2rem]"/>
       {/* 정보 받았을 때 이런 형태로 돌리기 */}
       {/* {Array.map((comment) => (
         <Comment />
       ))} */}
-      {/* <Comment /> */}
+      <Comment />
+
+      <div className="w-full flex flex-col gap-[1rem] items-end	">
+          <textarea className="resize-none	w-full h-[80px] border border-gray-300 outline-none	rounded-md" />
+          <button className="m-0 text-xs border-[1px] bg-gray-900 text-gray-50 rounded-full border-black text-sm px-[0.7rem] pt-[0.5rem] pb-[0.3rem] hover:bg-white hover:text-black duration-300">
+            댓글 달기
+          </button>
+        </div>
     </div>
   );
 }
--- frontend/src/routes/postDetail/component/Reply.tsx
+++ frontend/src/routes/postDetail/component/Reply.tsx
@@ -1,24 +1,22 @@
-import React from "react";
-import { AiOutlineEnter } from "react-icons/ai";
 
 // type Props = {};
 
 export default function Reply() {
   return (
-    <div className="ml-[2rem] grid grid-cols-12 gap-[1rem] items-center pl-[2rem] py-[2rem]">
-      <AiOutlineEnter className="scale-x-[-1] m-[1rem]" width="40px" />
-      <div className="col-[2_/_5] flex gap-[1rem] items-center">
-        <img
-          className="w-[40px] h-[40px] rounded-full"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTD68cSMsrBiEs6YloK8MVPO1DlJ7LqKt4OxT7ioMJn7xh-1iqPV0FVFjvTA7Cvlv-Y9Yc&usqp=CAU"
-        />
-        <p className="text-sm">댓글 단 user~~~</p>
-      </div>
-      <div className="col-[5_/_13] grow flex justify-between items-center bg-gray-300 rounded-2xl px-[1.25rem] py-[0.75rem]">
-        <p>오 글 좋네요 하트 누름^_^</p>
-        <button className="w-[46px] h-[30px] bg-black text-white rounded-full border-[1px] border-black text-xs px-[0.7rem] py-[0.3rem] hover:bg-white hover:text-black duration-300">
-          삭제
-        </button>
+    <div className="flex gap-[1rem] my-[36px] pl-[32px] w-full">
+      <div
+        style={{
+          backgroundImage: `url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTD68cSMsrBiEs6YloK8MVPO1DlJ7LqKt4OxT7ioMJn7xh-1iqPV0FVFjvTA7Cvlv-Y9Yc&usqp=CAU)`,
+        }}
+        className="w-[32px] h-[32px] rounded-full bg-cover bg-center cursor-pointer"
+      ></div>
+      <div style={{width: 'calc(100% - 32px)'}}>
+        <div className="mb-[1rem]">
+          <p className="text-sm">대댓글 단 user</p>
+          <p className="text-xs text-gray-500">Jun 13. 2024.</p>
+        </div>
+
+        <p className="mb-[1rem]">ㅇㅈㅇㅈ~~</p>
       </div>
     </div>
   );
