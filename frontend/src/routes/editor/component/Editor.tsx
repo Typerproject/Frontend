214e5096561f91afc30eaec7aaabe535bea9d098
lemonticsoul <sjho714@naver.com>
2024-06-25 13:03:58 +0900

    ✨feat: 이미지 블록 에디터 추가

--- frontend/src/routes/editor/component/Editor.tsx
+++ frontend/src/routes/editor/component/Editor.tsx
@@ -1,20 +1,21 @@
 import React, { useRef, useEffect } from "react";
 import EditorJS from "@editorjs/editorjs";
-import "../../../index.css";
 import Header from "@editorjs/header";
-import Paragraph from "@editorjs/paragraph";
-import { type ToolConstructable, OutputData } from "@editorjs/editorjs";
 import { ChartBLock } from "./blockTools/chart/ChartBlock";
 import { NewsBlock } from "./blockTools/news/NewsBlock";
 import { FinanceBlock } from "./blockTools/finance/FinanceBlock";
 import { ReportBlock } from "./blockTools/Report/ReportBlock";
+import { type ToolConstructable, OutputData } from "@editorjs/editorjs";
 import { DisclosureBlock } from "./blockTools/disclosure/DisclosureBlock";
+import { ImageBlock } from "./blockTools/image/ImageBlock";
 
 type Props = {
   setContent: (value: OutputData) => void;
 };
+
 export default function Editor({ setContent }: Props) {
   const ejInstance = useRef<EditorJS | null>(null);
+
   const initEditor = () => {
     const editor = new EditorJS({
       holder: "editorjs",
@@ -31,34 +32,64 @@ export default function Editor({ setContent }: Props) {
           class: Header as unknown as ToolConstructable,
           config: {
             placeholder: "Enter a header",
-            levels: [1, 2, 3, 4],
-            defaultLevel: 3,
+            levels: [1, 2, 3, 4, 5, 6],
+            defaultLevel: 1,
           },
         },
+      
+        image: ImageBlock,
         charts: ChartBLock,
         news: NewsBlock,
         Report: ReportBlock,
         finance: FinanceBlock,
         disclosure: DisclosureBlock,
-        paragraph: {
-          class: Paragraph,
-          config: {
-            preserveBlank: true,
-          },
-        },
       },
       autofocus: true,
     });
+
+    document.addEventListener('paste', handlePaste);
+
+    return () => {
+      document.removeEventListener('paste', handlePaste);
+      ejInstance?.current?.destroy();
+      ejInstance.current = null;
+    };
   };
+
+  const handlePaste = (event: ClipboardEvent) => {
+    event.preventDefault();
+    if (event.clipboardData) {
+      const items = event.clipboardData.items;
+      for (let i = 0; i < items.length; i++) {
+        if (items[i].type.indexOf('image') !== -1) {
+          const file = items[i].getAsFile();
+          if (file) {
+            const reader = new FileReader();
+            reader.onload = (e) => {
+              const url = e.target?.result as string;
+              insertImageBlock(url);
+            };
+            reader.readAsDataURL(file);
+          }
+        }
+      }
+    }
+  };
+
+  const insertImageBlock = (imageDataUrl: string) => {
+    if (ejInstance.current) {
+      ejInstance.current.blocks.insert('image', {
+          url: imageDataUrl
+      })
+    }
+  };
+
   useEffect(() => {
     if (ejInstance.current === null) {
       initEditor();
     }
-    return () => {
-      ejInstance?.current?.destroy();
-      ejInstance.current = null;
-    };
   }, []);
+
   return (
     <div
       id="editorjs"
